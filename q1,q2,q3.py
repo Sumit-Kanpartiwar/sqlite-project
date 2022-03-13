e65ff5d8b9bba29b2895b8688cfb4f48439a7dbd
Sumit-Kanpartiwar <98641781+Sumit-Kanpartiwar@users.noreply.github.com>
2022-03-13 10:19:47 +0530

    Update q1,q2,q3.py

--- q1,q2,q3.py
+++ q1,q2,q3.py
@@ -1,3 +1,8 @@
+"""    q1 = try to find out a count of each and every word in a respective file return a list of tuple with word and its respective count 
+       sample example -  [('sudh', 6 ) , ('kumar',3)]
+    q2 = try to perform a reduce operation to get a count of all the word starting with same alphabet
+        sample examle = [(a,56) , (b,34),...........]
+    q3 = Try to filter out all the words from dataset . """
 import csv
 import logging 
 logging.basicConfig(filename='task.log',level=logging.DEBUG)
